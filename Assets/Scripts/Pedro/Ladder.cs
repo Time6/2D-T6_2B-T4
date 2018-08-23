1bbae1f9b7563109b2362e1860f68ce4c85b6f51
Aluno <aluno@naverecife.local>
2018-08-23 09:57:37 -0300

    quase nada

--- Assets/Scripts/Pedro/Ladder.cs
+++ Assets/Scripts/Pedro/Ladder.cs
@@ -22,16 +22,19 @@ public class Ladder : MonoBehaviour {
         {
            player.rb.velocity = new Vector2 (0 , speedLadder);
             player.rb.gravityScale = 0;
+            Physics2D.IgnoreLayerCollision(8, 10, true);
         }
         else if(coll.tag == "Player" && Input.GetKey(KeyCode.DownArrow)) 
         {
              player.rb.velocity = new Vector2 (0 , -speedLadder);
               player.rb.gravityScale = 0;
+            Physics2D.IgnoreLayerCollision(8, 10, true);
         }
         else if (coll.tag == "Player")
         {
              player.rb.velocity = new Vector2 (0,0);
               player.rb.gravityScale = 0;
+            Physics2D.IgnoreLayerCollision(8, 10, true);
         }
         
     }
@@ -40,7 +43,8 @@ public class Ladder : MonoBehaviour {
      if (other.tag == "Player")
      {
           player.rb.gravityScale = 1;
-     }
+            Physics2D.IgnoreLayerCollision(8, 10, false);
+        }
         
     }
   
