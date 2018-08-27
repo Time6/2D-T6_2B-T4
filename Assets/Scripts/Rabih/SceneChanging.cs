dd8acafea3cfa3d96b7f7edac649af64ff6d1737
Aluno <aluno@naverecife.local>
2018-08-27 14:49:06 -0300

    pausando

---
+++ Assets/Scripts/Rabih/SceneChanging.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanging : MonoBehaviour
+{
+
+    public void sceneChange(string cena)
+    {
+        SceneManager.LoadScene(cena);
+    }
+}
