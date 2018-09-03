800d4b1bfacfe8b66f3ce41c2812c2869347fb8a
unknown <aluno@naverecife.local>
2018-09-03 14:58:55 -0300

    Contador

---
+++ Assets/Scripts/Rabih/Countdown.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+
+    // Use this for initialization
+    float seconds;
+    int minutes;
+    [SerializeField]Text timeUI;
+   
+
+
+	void Start () {
+
+        seconds = 60f;
+        minutes = 7;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        seconds -= Time.deltaTime;
+        if (seconds <= 0)
+        {
+            minutes -= 1;
+            seconds = 60f;
+        }
+        timeUI.text = minutes.ToString() + ":" + seconds.ToString();
+       
+
+	}
+}
