d9222fe906cc1651239b23c64c4bf95a0bb06be5
Miguel <time62D2018@gmail.com>
2018-10-02 08:15:26 -0300

    Project Patterns

---
+++ Assets/Scripts/Rabih/Project Patterns/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+
+    public GameObject player;
+
+
+	private static Singleton instance;
+
+	public static Singleton GetInstance{
+		get{
+			if(instance == null){
+				instance = GameObject.FindObjectOfType<Singleton>();
+			}
+			return instance;
+		}
+	}
+}
