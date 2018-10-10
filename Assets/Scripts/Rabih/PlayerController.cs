b767f9b1fb464e5f732927c92fb3eafa2f7af8fb
jos <aluno@naverecife.local>
2018-10-10 12:58:36 -0300

    escada quase consertada

--- Assets/Scripts/Rabih/PlayerController.cs
+++ Assets/Scripts/Rabih/PlayerController.cs
@@ -18,6 +18,13 @@ public class PlayerController : MonoBehaviour
     //nas escadas
    public float gettingUpSpeed;
 
+public float climbingCheckRadius;
+   private bool isClimbing;
+   private float axisY;
+   public LayerMask ladderMask;
+   public Transform ladderCheckStart;
+   public Transform ladderCheckEnd;
+
 
 
     public Rigidbody2D rb;
@@ -52,6 +59,8 @@ public class PlayerController : MonoBehaviour
     {
         GroundCheck();
         Movimentacao();
+        LadderCheck();
+        Climbing();
         if(talking)
         {
             rb.velocity = new Vector2(0,rb.velocity.y);
@@ -85,13 +94,39 @@ public class PlayerController : MonoBehaviour
         onGround = Physics2D.OverlapCircle(groundCheck.position, groundChecksize, platformMask);
     }
 
+    void LadderCheck()
+    {
+        Debug.DrawLine(ladderCheckStart.position, ladderCheckEnd.position , Color.red);
+   if(Physics2D.Linecast(ladderCheckStart.position, ladderCheckEnd.position , ladderMask))
+   {
+       
+   axisY = Input.GetAxis("Vertical");
+   }
+
+   }
+
+
+   void Climbing()
+   {
+
+    rb.velocity = new Vector2(rb.velocity.x, gettingUpSpeed * axisY);
+    isClimbing = Physics2D.OverlapCircle(transform.position , climbingCheckRadius,ladderMask);
+    
+
+   }
+
+    
+
     void OnDrawGizmos() {
 
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(groundCheck.position, groundChecksize);
+         Gizmos.DrawSphere(transform.position, climbingCheckRadius);
+
 
 
     }
+
 
     /*void OnTriggerEnter2D(BoxCollider2D coll)
     {
